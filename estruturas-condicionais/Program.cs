cc23e1619a9188de9129ac452cec41a453d04e90
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-06 17:28:26 -0400

    Aula 4-2

--- estruturas-condicionais/Program.cs
+++ estruturas-condicionais/Program.cs
@@ -1,12 +1,43 @@
-﻿double nota1 = 8;
-double nota2 = 8.0;
-double nota3 = 6.8;
-double media = (nota1 + nota2 + nota3) / 3;
-if (media >= 7)
+﻿//double nota1 = 8;
+//double nota2 = 8.0;
+//double nota3 = 6.8;
+//double mediaNota = (nota1 + nota2 + nota3) / 3;
+//if (mediaNota >= 7)
+//{
+//    Console.WriteLine($"Média {mediaNota:F2}: Aprovado");
+//}
+//else if(mediaNota >= 5 && mediaNota < 7)
+//{
+//    Console.WriteLine($"Média {mediaNota:F2}: Recuperação");
+//}
+//else
+//{
+//    Console.WriteLine($"Média {mediaNota:F2}: Aprovado");
+//}
+
+Console.WriteLine("Digite uma vogal: ");
+string? letra = Console.ReadLine();
+if (letra == "a" || letra == "A")
 {
-    Console.WriteLine($"Média {media:F2}: Aprovado");
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if (letra == "e" || letra == "E")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if (letra == "i" || letra == "I")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if (letra == "o" || letra == "O")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if (letra == "u" || letra == "U")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
 }
 else
 {
-    Console.WriteLine($"Média {media:F2}: Aprovado");
+    Console.WriteLine($"A letra {letra} não é uma vogal.");
 }

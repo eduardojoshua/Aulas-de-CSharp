3f22cfca17ec2667694a2eca411718c7ffacdcc8
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-06 16:28:38 -0400

    Aula 4

---
+++ estruturas-condicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿double nota1 = 8;
+double nota2 = 8.0;
+double nota3 = 6.8;
+double media = (nota1 + nota2 + nota3) / 3;
+if (media >= 7)
+{
+    Console.WriteLine($"Média {media:F2}: Aprovado");
+}
+else
+{
+    Console.WriteLine($"Média {media:F2}: Aprovado");
+}

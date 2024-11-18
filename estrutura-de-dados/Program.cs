c5d822900c857c205bda75c476fbc00d908dd230
eduardojoshua <161325110+eduardojoshua@users.noreply.github.com>
2024-11-18 16:01:13 -0400

    feijao

--- estrutura-de-dados/Program.cs
+++ estrutura-de-dados/Program.cs
@@ -7,3 +7,14 @@
 
 int[] numeros = {1,2,3,4,5};
 
+int soma = 0;
+
+foreach (int numero in numeros)
+{
+    soma += numero;
+}
+
+Console.WriteLine($"A soma dos numeros é {soma}");
+
+// Exercício 2
+
